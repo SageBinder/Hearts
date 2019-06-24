0a3965907b44cedcc44242faada75726f101b5e1
SageBinder <sagebin13@gmail.com>
2019-06-23 18:10:28 -0700

    Ugh trying to get this shit to work on MacOS

--- desktop/src/com/sage/hearts/desktop/DesktopLauncher.java
+++ desktop/src/com/sage/hearts/desktop/DesktopLauncher.java
@@ -7,6 +7,7 @@ import com.sage.hearts.client.HeartsGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		System.setProperty("org.lwjgl.opengl.Display.allowSoftwareOpenGL", "true");
 		config.samples = 16;
 		config.forceExit = false;
 		new LwjglApplication(new HeartsGame(), config);
